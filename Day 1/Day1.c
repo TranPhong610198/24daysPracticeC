cd8bb874c331a819bef94fa4af4fee000ffbd791
Moriarty <tphong610198@gmail.com>
2023-10-12 14:30:06 +0700

    commited by Moriarty

---
+++ Day 1/Day1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int main(){ 
+    // Bài 1 : Tính và in ra phần nguyên và phần dư của phép chia 2 số a và b nhập từ bàn phím
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d %d", a/b, a%b);
+
+    // Bài 2 : Tính giá trị biểu thức 1 : A = x^3 + 3x^2 + x + 1;
+    // int x;
+    // scanf("%d", &x);
+    // printf("%.0lf", (pow(x,3) + (3*x*x) + x + 1 ));
+
+    // Bài 3 : Tính giá trị biếu thức 2 : a*(b+c) + b*(a+c)
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
+    // printf("%d", a*(b+c) + b*(a+c));
+
+    // Bài 4: Tính tổng hiệu tích thương 
+    // double a, b;
+    // scanf("%lf %lf", &a, &b);
+    // printf("%.0lf %.0lf %.0lf %.2lf", a+b, a-b, a*b, a/b); 
+
+    // Bài 5: Tính chu vi, diện tích hình tròn
+    // float r ;
+    // scanf("%f", &r);
+    // printf("%.2f %.2f",2*3.14*r ,3.14*r*r);
+
+    // Bài 6: Tính Khoảng Cách Hai Điểm Trong Tọa Độ Oxy
+    // int x1 , y1, x2, y2;
+    // float d;
+    // scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    // d = sqrt(pow((x1-x2),2) + pow((y1-y2),2));
+    // printf("%.2f", d);
+
+    // Bài 7: Chuyển đơn vị đo nhiệt độ C và F
+    // float c,f;
+    // scanf ("%f", &c);
+    // printf("%.2f", (c*9/5)+32);
+
+    // Bài 8: Tính Tổng Sn=1+2+3+...+n;
+    // long long n;
+    // scanf("%lld", &n);
+    // printf("%lld", ((n+1)*n)/2);
+
+    // Bài 9: Tính Tổng Sn = 1^2 + 2^2 + ... + n^2
+    long long n;
+    scanf("%lld", &n);
+    long long d = (n * (n+1)(2 * n+1)/6);
+    printf("%lld",d);
+}
